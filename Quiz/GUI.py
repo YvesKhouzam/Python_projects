44e893597008c48336bc455463c56923622a7926
KhouYv1 <yveskhouzam@gmail.com>
2020-12-02 22:15:41 -0500

    From Pycharm_Yoga

--- Quiz/GUI.py
+++ Quiz/GUI.py
@@ -8,12 +8,21 @@ root.iconphoto(False, tk.PhotoImage(file="quiz.png"))
 titre = tk.Label(text="Quiz sur les polynômes", bg="grey", fg="black", width=50)
 titre.pack()
 
+def check_answer():
+    print("OK")
+
 score = 0
 num_question = 0
-question1 = tk.Label(text='Devinez la figure géométrique')
+question = tk.Label(text='Devinez la figure géométrique')
+question.pack()
+question1 = tk.Label(text='Une figure à trois côtés s\'appelle: ')
 question1.pack()
-#guess1 = tk.Entry('Une figure à trois côtés s\'appelle: ')
-#guess1.pack()
+guess1 = tk.Entry(root)
+guess1.pack()
+valide = tk.Button(root, text="Valide ta réponse", command=check_answer)
+valide.pack()
+
+
 # check_guess(guess1, 'triangle')
 
 
